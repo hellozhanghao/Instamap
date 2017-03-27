13149a8eb8de159adbe56daa781c80be7625844a
Zhang Hao <zhangghaoo@gmail.com>
2017-03-27 20:01:54 +0800

    add crawler

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 client_id ="b5c8af1bee0f44b8b0054a329107de0a"
 client_secret = "6ed3f0a35db34481afb68a7fb3a9aac9"
+
 from instagram.client import InstagramAPI
 
 api = InstagramAPI(client_id=client_id, client_secret=client_secret)
